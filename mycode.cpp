e6ec70bd1ca7447379f77a8028450d7842016bfb
jokereactive <sarthakahuja@outlook.com>
2015-05-11 01:03:28 +0530

    completed the driver code

--- mycode.cpp
+++ mycode.cpp
@@ -5,9 +5,19 @@ int main() {
 
   cout << "(x^0)/0! + (x^1)/1! + (x^2)/2! + (x^3)/3! + (x^4)/4! + ........ + (x^n)/n! " << endl;
 
-  //Take Input
-  
-  //Give Output
+  cout << "Please enter the value of x : " ;
+
+  double x;
+  cin >> x;
+
+  int n;
+  cout << endl << "Please enter an integer value for n : " ;
+  cin >> n;
+  cout << endl;
+
+  double seriesValue = ComputeSeriesValue(x, n);
+  cout << "The value of the series for the values entered is "
+	<< seriesValue << endl;
 
   return 0;
 }
