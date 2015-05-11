1a0618cbc31435cae57735ac7e54cb9257229170
shreyatrip <shreya1424@iiitd.ac.in>
2015-05-11 12:24:36 +0530

    changed line

--- mycode.cpp
+++ mycode.cpp
@@ -1,7 +1,7 @@
 #include "myheader.h"
 
 int main() {
-  cout << "This program is used to compute the value of the following series : " << endl;
+  cout << "Value of series " << endl;
 
   cout << "(x^0)/0! + (x^1)/1! + (x^2)/2! + (x^3)/3! + (x^4)/4! + ........ + (x^n)/n! " << endl;
 
